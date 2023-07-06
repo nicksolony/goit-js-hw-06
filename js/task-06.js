8ba10806aac833e7b18b7ceec1de2d90c07c9e82
nicksolony <nicksolony@gmail.com>
2023-07-06 19:52:44 -0400

    task 6 completed

--- js/task-06.js
+++ js/task-06.js
@@ -1,10 +1,15 @@
 let inputEl = document.querySelector('#validation-input');
 
 inputEl.addEventListener('blur',(e)=>{
-    console.log(e.target.value.length);
-    if (e.target.value.length === parseInt(inputEl.getAttribute('data-length'))) {
-        inputEl.classList.add('valid')
+    // console.log(e.target.value.length);
+
+    // console.log(parseInt(e.target.value.length)===parseInt(inputEl.getAttribute('data-length')))
+
+    if (parseInt(e.target.value.length)===parseInt(inputEl.getAttribute('data-length'))) {
+        inputEl.classList.add('valid');
+        inputEl.classList.remove('invalid');
     } else {
-        inputEl.classList.add('invalid')
+        inputEl.classList.add('invalid');
+        inputEl.classList.remove('valid');
     }
 })
