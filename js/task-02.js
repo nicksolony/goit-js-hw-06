67aefb5aef7d975cc4d1ef1c1fdf4c9641fa47fc
nicksolony <nicksolony@gmail.com>
2023-07-06 17:05:13 -0400

    task 2 complted

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+let ingredientsList= document.querySelector("#ingredients");
+// console.log(ingredientsList);
+
+
+function addIngridientEl(ingridient) {
+  let ingredientEl = document.createElement('li');
+  ingredientEl.innerText = ingridient;
+  ingredientEl.classList.add('item');
+  return ingredientEl;
+}
+
+let ingredientEls = ingredients.map(addIngridientEl);
+
+ingredientsList.append(...ingredientEls);
