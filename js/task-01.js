32192cceaaec5b1fe1b919073cb89b64b3bb84c7
nicksolony <nicksolony@gmail.com>
2023-07-06 16:54:04 -0400

    task 1 completed

--- js/task-01.js
+++ js/task-01.js
@@ -1,12 +1,9 @@
 let categoriesEl = document.querySelectorAll("ul#categories li.item");
 console.log(`Number of categories: ${categoriesEl.length}`);
 
-// categoriesEl.map((categoryEl) => console.log(categoryEl.querySelector("h2"))
-//     )
-// console.log(categoriesEl);
+function getCategoryEl(category) {
+    console.log(`Category: ${category.querySelector("h2").textContent}`)
+    console.log(`Elements: ${category.querySelectorAll("li").length}`)
+}
 
-categoriesEl.forEach( (categoryEl) =>
-    console.log(categoryEl.querySelectorAll("li"));
-    console.log(`Category: ${categoryEl.querySelector("h2").textContent}`)
-    // console.log(`Elements: ${categoryEl.querySelectorAll("li")}`)
-    );
+categoriesEl.forEach(getCategoryEl);
