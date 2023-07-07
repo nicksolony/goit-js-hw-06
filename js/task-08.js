b54477859f2af84805f47a67875705d42911aaba
nicksolony <nicksolony@gmail.com>
2023-07-06 20:56:23 -0400

    task 8 completed

--- js/task-08.js
+++ js/task-08.js
@@ -5,12 +5,14 @@ let formEl= document.querySelector('.login-form');
 formEl.addEventListener('submit', (e)=> {
     e.preventDefault();
     
-    if (condition) {
-        
-    } else {
-        
-    }
+    let email = e.target.elements['email'].value;
+    let password = e.target.elements['password'].value;
 
-    console.log(e.target.querySelector('[name="email"]').value);
-    console.log(e.target.querySelector('[name="password"]').value);
+    if ( !email || !password) {
+        alert('All fields must be filled in!')
+    } else {
+        let loginIngo = {email,password};
+        console.log(loginIngo);
+        e.target.reset();    
+    }
 })
