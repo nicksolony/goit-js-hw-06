b912d2e15ffd5e158efcb5f0f0be47f9616fdac7
nicksolony <nicksolony@gmail.com>
2023-07-06 20:17:44 -0400

    started working on task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+let formEl= document.querySelector('.login-form');
 
+
+
+formEl.addEventListener('submit', (e)=> {
+    e.preventDefault();
+    
+    if (condition) {
+        
+    } else {
+        
+    }
+
+    console.log(e.target.querySelector('[name="email"]').value);
+    console.log(e.target.querySelector('[name="password"]').value);
+})
